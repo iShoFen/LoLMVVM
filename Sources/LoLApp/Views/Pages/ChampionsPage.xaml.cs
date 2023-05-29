1a8afd6a719fd4ff2403f3a8a10b8a7ffdfd3dee
Samuel SIRVEN <samuel.sirven@etu.uca.fr>
2023-05-29 22:05:02 +0200

    💄add the champion page

---
+++ Sources/LoLApp/Views/Pages/ChampionsPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using ViewModel;
+
+namespace LoLApp.Views.Pages;
+
+public partial class ChampionsPage : ContentPage
+{
+    
+    private ChampionMgrVM MgrVm { get; set; }
+    
+    public ChampionsPage(ChampionMgrVM mgrVm)
+    {
+        MgrVm = mgrVm;
+        InitializeComponent();
+        BindingContext = MgrVm;
+    }
+}
