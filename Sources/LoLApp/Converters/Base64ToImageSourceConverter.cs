a7487046c2d2095d8bb52df9bf96265dd16dad65
Samuel SIRVEN <samuel.sirven@etu.uca.fr>
2023-05-29 22:05:20 +0200

    👔 add the base64 converter

---
+++ Sources/LoLApp/Converters/Base64ToImageSourceConverter.cs
@@ -0,0 +1,32 @@
+﻿using System.Globalization;
+using CommunityToolkit.Maui.Converters;
+
+namespace LoLApp.Converters;
+
+public class Base64ToImageSourceConverter: ByteArrayToImageSourceConverter, IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is not string base64) return null;
+        
+        try
+        {
+            var bytes = System.Convert.FromBase64String(base64);
+                
+            return ConvertFrom(bytes);
+        }
+        catch
+        {
+            return null;
+        }
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is not ImageSource imageSource) return null;
+        
+        var bytes = ConvertBackTo(imageSource);
+
+        return bytes != null ? System.Convert.ToBase64String(bytes) : null;
+    }
+}
