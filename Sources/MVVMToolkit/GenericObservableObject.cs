a829d271ff566da2e59c5842beb0990c73db89d1
Samuel SIRVEN <samuel.sirven@etu.uca.fr>
2023-05-29 22:05:33 +0200

    👔 add the mvvm toolkit

---
+++ Sources/MVVMToolkit/GenericObservableObject.cs
@@ -0,0 +1,22 @@
+﻿namespace MVVMToolkit;
+
+/// <summary>
+/// A generic version of <see cref="ObservableObject"/> That define a <see cref="Model"/> property
+/// </summary>
+/// <typeparam name="T">The type of the model</typeparam>
+public abstract class ObservableObject<T> : ObservableObject
+{
+    /// <summary>
+    /// The model
+    /// </summary>
+    protected T Model { get; }
+    
+    /// <summary>
+    /// Create a new instance of <see cref="ObservableObject{T}"/>
+    /// </summary>
+    /// <param name="model"></param>
+    protected ObservableObject(T model)
+    {
+        Model = model;
+    }
+}
