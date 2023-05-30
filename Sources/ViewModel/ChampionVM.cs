5575a78ebfe8719b3d79ad0ba04e8d1a953ccb98
Samuel SIRVEN <samuel.sirven@etu.uca.fr>
2023-05-30 03:28:03 +0200

    👔  update the champion vm with the new props

--- Sources/ViewModel/ChampionVM.cs
+++ Sources/ViewModel/ChampionVM.cs
@@ -1,4 +1,5 @@
-﻿using Model;
+﻿using System.Collections.ObjectModel;
+using Model;
 using MVVMToolkit;
 
 namespace ViewModel;
@@ -32,5 +33,78 @@ public class ChampionVM: ObservableObject<Champion>
         set => SetProperty(Model.Class, value, Model, (model, value) => model.Class = value);
     }
     
-    public ChampionVM(Champion model) : base(model) { }
+    public ReadOnlyObservableCollection<SkinVM> Skins { get; private set; }
+    private readonly ObservableCollection<SkinVM> skins;
+
+    public ReadOnlyObservableDictionary<string, int> Characteristics { get; private set; }
+    private readonly ObservableDictionary<string, int> characteristics;
+    
+    public ReadOnlyObservableCollection<SkillVM> Skills { get; private set; }
+    private readonly ObservableCollection<SkillVM> skills;
+
+    public ChampionVM(Champion model) : base(model)
+    {
+        skins = new ObservableCollection<SkinVM>(model.Skins.Select(skin => new SkinVM(skin)));
+        Skins = new ReadOnlyObservableCollection<SkinVM>(skins);
+        
+        characteristics = new ObservableDictionary<string, int>(model.Characteristics);
+        Characteristics = new ReadOnlyObservableDictionary<string, int>(characteristics);
+        
+        skills = new ObservableCollection<SkillVM>(model.Skills.Select(skill => new SkillVM(skill)));
+        Skills = new ReadOnlyObservableCollection<SkillVM>(skills);
+    }
+    
+    internal bool AddSkin(SkinVM skin)
+    {
+        if (!Model.AddSkin(skin.Model)) return false;
+        
+        skins.Add(skin);
+        
+        return true;
+    }
+    
+    internal bool RemoveSkin(SkinVM skin)
+    {
+        if (!Model.RemoveSkin(skin.Model)) return false;
+        
+        skins.Remove(skin);
+        
+        return true;
+    }
+    
+    public bool AddSkill(SkillVM skill)
+    {
+        if (!Model.AddSkill(skill.Model)) return false;
+        
+        skills.Add(skill);
+        
+        return true;
+    }
+    
+    public bool RemoveSkill(SkillVM skill)
+    {
+        if (!Model.RemoveSkill(skill.Model)) return false;
+        
+        skills.Remove(skill);
+        
+        return true;
+    }
+    
+    public void AddCharacteristics(params Tuple<string, int>[] someCharacteristics)
+    {
+        Model.AddCharacteristics(someCharacteristics);
+        foreach (var chara in someCharacteristics)
+        {
+            characteristics[chara.Item1] = chara.Item2;
+        }
+    }
+    
+    public bool RemoveCharacteristic(string label)
+    {
+        if (!characteristics.ContainsKey(label)) return false;
+        
+        characteristics.Remove(label);
+        
+        return true;
+    }
 }
