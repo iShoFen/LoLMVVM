9c92cca43367b737db5a2fc25f04642cefc7e6c3
Samuel SIRVEN <samuel.sirven@etu.uca.fr>
2023-05-30 03:26:31 +0200

    👔  add the skill vm

---
+++ Sources/ViewModel/SkillVM.cs
@@ -0,0 +1,21 @@
+﻿using Model;
+using MVVMToolkit;
+
+namespace ViewModel;
+
+public class SkillVM: ObservableObject<Skill>
+{
+    internal new Skill Model => base.Model;
+    
+    public string Name => Model.Name;
+    
+    public SkillType Type => Model.Type;
+
+    public string Description
+    {
+        get => Model.Description;
+        set => SetProperty(Model.Description, value, Model, (model, value) => model.Description = value);
+    }
+    
+    public SkillVM(Skill model) : base(model) { }
+}
