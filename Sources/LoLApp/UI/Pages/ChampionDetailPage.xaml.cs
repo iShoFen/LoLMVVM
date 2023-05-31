b6e63a6c749e89e90a9bd60dca1ac183349268b2
Samuel SIRVEN <samuel.sirven@etu.uca.fr>
2023-05-31 03:54:20 +0200

    💄start the detail page
    - Add the DetailPage
    - Add the custom TitleView with back button, and update
    - Extract a ChampionDetailSummary View

---
+++ Sources/LoLApp/UI/Pages/ChampionDetailPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using ViewModel;
+
+namespace LoLApp.UI.Pages;
+
+public partial class ChampionDetailPage : ContentPage
+{
+    private ChampionMgrVM MgrVm { get; set; }
+    
+    public ChampionDetailPage(ChampionMgrVM mgrVm)
+    {
+        MgrVm = mgrVm;
+        InitializeComponent();
+        BindingContext = MgrVm;
+    }
+
+    private void Button_OnClicked(object sender, EventArgs e)
+    {
+        Navigation.PopAsync();
+    }
+}
+
---
+++ Sources/LoLApp/UI/Views/ChampionDetailSummary.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoLApp.UI.Views;
+
+public partial class ChampionDetailSummary : ContentView
+{
+    public string Base64Image
+    {
+        get => (string)GetValue(Base64ImageProperty);
+        set => SetValue(Base64ImageProperty, value);
+    }
+    public static readonly BindableProperty Base64ImageProperty =
+        BindableProperty.Create(nameof(Base64Image), typeof(string), typeof(ChampionDetailSummary));
+    
+    public string Name 
+    {
+        get => (string)GetValue(NameProperty);
+        set => SetValue(NameProperty, value);
+    }
+    public static readonly BindableProperty NameProperty =
+        BindableProperty.Create(nameof(Name), typeof(string), typeof(ChampionDetailSummary));
+    
+    public string Class 
+    {
+        get => (string)GetValue(ClassProperty);
+        set => SetValue(ClassProperty, value);
+    }
+    public static readonly BindableProperty ClassProperty =
+        BindableProperty.Create(nameof(Class), typeof(string), typeof(ChampionDetailSummary));
+    
+    public string Bio
+    {
+        get => (string)GetValue(BioProperty);
+        set => SetValue(BioProperty, value);
+    }
+    public static readonly BindableProperty BioProperty =
+        BindableProperty.Create(nameof(Bio), typeof(string), typeof(ChampionDetailSummary));
+
+    public ChampionDetailSummary()
+    {
+        InitializeComponent();
+    }
+}
+
