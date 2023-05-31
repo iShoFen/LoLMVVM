20f368fbd30e9bfbef39211d862d218e9436e9d9
Samuel SIRVEN <samuel.sirven@etu.uca.fr>
2023-05-31 03:51:33 +0200

    👔 update champion mgr vm
    - Add on IsOrderedDescending prop for order the list
    - Update the LoaddChamion to use the orderd loading
    - add  a SelectedChampion for the detail page

--- Sources/ViewModel/ChampionMgrVM.cs
+++ Sources/ViewModel/ChampionMgrVM.cs
@@ -30,42 +30,68 @@ public class ChampionMgrVM: ObservableObject<IDataManager>
     }
     private int count = 5;
     
+    public bool IsOrderedDescending
+    {
+        get => isOrderedDescending;
+        set => SetProperty(ref isOrderedDescending, value);
+    }
+    private bool isOrderedDescending;
+    
+    public ChampionVM? SelectedChampion
+    {
+        get => selectedChampion;
+        set => SetProperty(ref selectedChampion, value);
+    }
+    private ChampionVM? selectedChampion;
+
+
     public ICommand LoadChampionsCommand { get; }
+    
+    public ICommand SortChampionCommand { get; }
     
     public ICommand PreviousPageCommand { get; }
 
     public ICommand NextPageCommand { get; }
-
+    
     public ChampionMgrVM(IDataManager model) : base(model)
     {
         Champions = new ReadOnlyObservableCollection<ChampionVM>(champions);
         PropertyChanged += LocalOnPropertyChanged;
-
-        // Only done to avoid the warning for (using async on void method is not recommended)
-        async void Execute() => await LoadChampions();
-        LoadChampionsCommand = new Command(Execute, () => true);
+        
+        LoadChampionsCommand = new Command(ExecuteLoad);
+        SortChampionCommand = new Command(ExecuteSort);
         
         PreviousPageCommand = new Command(() => Index = index - 1, () => index > 0);
         NextPageCommand = new Command(() => Index = index + 1, () => index < Page - 1);
     }
-
+    
     private async void LocalOnPropertyChanged(object? sender, PropertyChangedEventArgs e)
     {
         if (e.PropertyName is not (nameof(Index) or nameof(Count))) return;
-        
+
         await LoadChampions();
         (PreviousPageCommand as Command)?.ChangeCanExecute();
         (NextPageCommand as Command)?.ChangeCanExecute();
     }
-
+    
+    // Only done to avoid the warning for (using async on void method is not recommended)
+    private async void ExecuteLoad() => await LoadChampions();
     private async Task LoadChampions()
     {
         champions.Clear();
-        var champs = await Model.ChampionsMgr.GetItems(index, count);
-        // select only the champions that are not null
+        var champs = await Model.ChampionsMgr.GetItems(index, count, nameof(Champion.Name), IsOrderedDescending);
         foreach (var champion in champs.Where(champion => champion != null))
         {
             champions.Add(new ChampionVM(champion!));
         }
     }
+    
+    // Only done to avoid the warning for (using async on void method is not recommended)
+    private async void ExecuteSort() => await SortChampion();
+    private async Task SortChampion()
+    {
+        IsOrderedDescending = !IsOrderedDescending;
+        await LoadChampions();
+    }
+    
 }
