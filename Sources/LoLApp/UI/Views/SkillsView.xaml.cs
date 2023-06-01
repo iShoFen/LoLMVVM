ab7d4c00fcfd695926292f9b4ed823fa5b62abc6
Samuel SIRVEN <samuel.sirven@etu.uca.fr>
2023-06-01 02:14:13 +0200

    💄add the skills view
    PS Column Width has been fixed to 300 manualy for now, need to find a way for having a proper 80/20 display

---
+++ Sources/LoLApp/UI/Views/SkillsView.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ViewModel;
+
+namespace LoLApp.UI.Views;
+
+public partial class SkillsView : ContentView
+{
+    public IEnumerable<SkillVM> Skills
+    {
+        get => (IEnumerable<SkillVM>)GetValue(SkillsProperty);
+        set => SetValue(SkillsProperty, value);
+    }
+    public static readonly BindableProperty SkillsProperty =
+        BindableProperty.Create(nameof(Skills), typeof(IEnumerable<SkillVM>), typeof(SkillsView));
+
+    public SkillsView()
+    {
+        InitializeComponent();
+    }
+}
+
