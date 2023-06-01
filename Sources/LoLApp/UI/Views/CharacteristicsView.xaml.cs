b1cb2df5f2e49fc0d0c5790dc20f7b8dcf39131e
Samuel SIRVEN <samuel.sirven@etu.uca.fr>
2023-06-01 02:13:28 +0200

    💄 add the characteristics view

---
+++ Sources/LoLApp/UI/Views/CharacteristicsView.xaml.cs
@@ -0,0 +1,18 @@
+﻿namespace LoLApp.UI.Views;
+
+public partial class CharacteristicsView : ContentView
+{
+    public IDictionary<string, int> Characteristics
+    {
+        get => (IDictionary<string, int>)GetValue(CharacteristicsProperty);
+        set => SetValue(CharacteristicsProperty, value);
+    }
+    public static readonly BindableProperty CharacteristicsProperty =
+        BindableProperty.Create(nameof(Characteristics), typeof(IDictionary<string, int>), typeof(CharacteristicsView));
+    
+    public CharacteristicsView()
+    {
+        InitializeComponent();
+    }
+}
+
