a5ebdeb13786c2ce8d200ffd8b5b38e028ddaefb
Samuel SIRVEN <samuel.sirven@etu.uca.fr>
2023-06-02 02:26:05 +0200

    💄add the skin view

---
+++ Sources/LoLApp/UI/Views/SkinsViews.xaml.cs
@@ -0,0 +1,20 @@
+﻿using ViewModel;
+
+namespace LoLApp.UI.Views;
+
+public partial class SkinsViews : ContentView
+{
+    public IEnumerable<SkinVM> Skins
+    {
+        get => (IEnumerable<SkinVM>)GetValue(SkinsProperty);
+        set => SetValue(SkinsProperty, value);
+    }
+    public static readonly BindableProperty SkinsProperty = 
+        BindableProperty.Create(nameof(Skins), typeof(IEnumerable<SkinVM>), typeof(SkinsViews));
+    
+    public SkinsViews()
+    {
+        InitializeComponent();
+    }
+}
+
