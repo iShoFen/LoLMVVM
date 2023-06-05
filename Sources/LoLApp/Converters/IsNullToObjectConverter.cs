7899ddec8761967ee9836f350d9b441703fdacd3
Samuel SIRVEN <samuel.sirven@etu.uca.fr>
2023-06-05 05:35:12 +0200

    💄add the add champion page
    - Add a converter to convert Nullability to an Object
    - Add the navigation button

---
+++ Sources/LoLApp/Converters/IsNullToObjectConverter.cs
@@ -0,0 +1,20 @@
+﻿#nullable enable
+using System.Globalization;
+
+namespace LoLApp.Converters;
+
+
+public class IsNullToObjectConverter : IsNullToObjectConverter<object> { }
+
+public class IsNullToObjectConverter<TObject>: IValueConverter
+{
+    public TObject? TrueObject { get; set; }
+    
+    public TObject? FalseObject { get; set; }
+
+    public object? Convert(object? value, Type targetType, object parameter, CultureInfo culture)
+        => value is null ? TrueObject : FalseObject;
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) 
+        => throw new NotImplementedException();
+}
---
+++ Sources/LoLApp/UI/Pages/AddChampionPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using LoLApp.ViewModel;
+
+namespace LoLApp.UI.Pages;
+
+public partial class AddChampionPage : ContentPage
+{
+    private ApplicationVM AppVM { get; set; }
+    
+    public AddChampionPage(ApplicationVM appVM)
+    {
+        AppVM = appVM;
+        InitializeComponent();
+        BindingContext = AppVM;
+    }
+    
+    protected override bool OnBackButtonPressed()
+    {
+        AppVM.CancelCommand.Execute(null);
+        return true;
+    }
+}
+
