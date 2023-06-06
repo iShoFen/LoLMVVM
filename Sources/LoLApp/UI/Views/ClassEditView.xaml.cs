6dbbfa7ad05aee6fdab56f9e54e66159bdfa914a
Samuel SIRVEN <samuel.sirven@etu.uca.fr>
2023-06-06 23:13:18 +0200

    💄create tje class edit view and update the edit view

---
+++ Sources/LoLApp/UI/Views/ClassEditView.xaml.cs
@@ -0,0 +1,18 @@
+﻿namespace LoLApp.UI.Views;
+
+public partial class ClassEditView : ContentView
+{
+    public Enum SelectedItem
+    {
+        get => (Enum)GetValue(SelectedItemProperty);
+        set => SetValue(SelectedItemProperty, value);
+    }
+    public static readonly BindableProperty SelectedItemProperty =
+        BindableProperty.Create(nameof(SelectedItem), typeof(Enum), typeof(ClassEditView), default, BindingMode.TwoWay);
+    
+    public ClassEditView()
+    {
+        InitializeComponent();
+    }
+}
+
