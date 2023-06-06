59d78f06fdd9ae15f84f99db7c56f3689cfe8661
Samuel SIRVEN <samuel.sirven@etu.uca.fr>
2023-06-06 03:02:38 +0200

    💄 create components
    - Create PlusButton Components 
    - Create RoundEntry Components

---
+++ Sources/LoLApp/UI/Components/PlusButton.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Windows.Input;
+
+namespace LoLApp.UI.Components;
+
+public partial class PlusButton : ContentView
+{
+    public Color Color
+    {
+        get => (Color)GetValue(ColorProperty);
+        set => SetValue(ColorProperty, value);
+    }
+    public static readonly BindableProperty ColorProperty = 
+        BindableProperty.Create(nameof(Color), typeof(Color), typeof(PlusButton), Colors.Black);
+    
+    public ICommand Command
+    {
+        get => (ICommand)GetValue(CommandProperty);
+        set => SetValue(CommandProperty, value);
+    }
+    public static readonly BindableProperty CommandProperty = 
+        BindableProperty.Create(nameof(Command), typeof(ICommand), typeof(PlusButton));
+    
+    public object CommandParameter
+    {
+        get => GetValue(CommandParameterProperty);
+        set => SetValue(CommandParameterProperty, value);
+    }
+    public static readonly BindableProperty CommandParameterProperty = 
+        BindableProperty.Create(nameof(CommandParameter), typeof(object), typeof(PlusButton));
+
+    public PlusButton()
+    {
+        InitializeComponent();
+    }
+}
+
---
+++ Sources/LoLApp/UI/Components/RoundEntry.xaml.cs
@@ -0,0 +1,34 @@
+﻿namespace LoLApp.UI.Components;
+
+public partial class RoundEntry : ContentView
+{
+    public string Placeholder
+    {
+        get => (string)GetValue(PlaceholderProperty);
+        set => SetValue(PlaceholderProperty, value);
+    }
+    public static readonly BindableProperty PlaceholderProperty = 
+        BindableProperty.Create(nameof(Placeholder), typeof(string), typeof(RoundEntry), string.Empty);
+    
+    public string Text
+    {
+        get => (string)GetValue(TextProperty);
+        set => SetValue(TextProperty, value);
+    }
+    public static readonly BindableProperty TextProperty = 
+        BindableProperty.Create(nameof(Text), typeof(string), typeof(RoundEntry), string.Empty, BindingMode.TwoWay);
+    
+    public Keyboard Keyboard
+    {
+        get => (Keyboard)GetValue(KeyboardProperty);
+        set => SetValue(KeyboardProperty, value);
+    }
+    public static readonly BindableProperty KeyboardProperty = 
+        BindableProperty.Create(nameof(Keyboard), typeof(Keyboard), typeof(RoundEntry), Keyboard.Default);
+    
+    public RoundEntry()
+    {
+        InitializeComponent();
+    }
+}
+
