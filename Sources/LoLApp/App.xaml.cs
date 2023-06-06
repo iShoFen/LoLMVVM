f1c5d87b3791ce643e8bc715b4dd62f09651fc92
Samuel SIRVEN <samuel.sirven@etu.uca.fr>
2023-06-06 03:02:52 +0200

    💄update the android entry with custom colors

--- Sources/LoLApp/App.xaml.cs
+++ Sources/LoLApp/App.xaml.cs
@@ -1,11 +1,37 @@
-﻿namespace LoLApp;
+﻿using Microsoft.Maui.Controls.PlatformConfiguration;
+using Microsoft.Maui.Platform;
+
+namespace LoLApp;
 
 public partial class App : Application
 {
 	public App()
 	{
 		InitializeComponent();
+		InitStyleHandlers();
 
 		MainPage = new AppShell();
 	}
+
+	private static void InitStyleHandlers()
+	{
+		Microsoft.Maui.Handlers.EntryHandler.Mapper.AppendToMapping("LoLCustomization", (handler, _) =>
+			{
+				#if __ANDROID__
+					var primaryColor = (Color) Resources.MergedDictionaries.First()["Primary"];
+					handler.PlatformView.TextCursorDrawable!.SetTint(primaryColor.ToPlatform());
+					handler.PlatformView.SetBackgroundColor(Colors.Transparent.ToPlatform());
+				#endif
+			}
+		);
+
+		Microsoft.Maui.Handlers.EditorHandler.Mapper.AppendToMapping("LoLCustomization", (handler, _) =>
+			{
+				#if __ANDROID__
+					handler.PlatformView.SetBackgroundColor(Colors.Transparent.ToPlatform());
+				#endif
+			}
+		);
+	}
 }
+	
