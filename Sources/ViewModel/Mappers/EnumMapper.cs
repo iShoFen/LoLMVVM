5b618cbf5e3148447a3f54d32481239b2556446f
Samuel SIRVEN <samuel.sirven@etu.uca.fr>
2023-06-09 02:46:57 +0200

    👔 add the specific mappers and extensions for the enums

---
+++ Sources/ViewModel/Mappers/EnumMapper.cs
@@ -0,0 +1,34 @@
+﻿using Model;
+using Shared.Mappers;
+using ViewModel.Enums;
+
+namespace ViewModel.Mappers;
+
+/// <summary>
+/// A static class to hold the mappers
+/// </summary>
+internal static class EnumsMapper
+{
+    /// <summary>
+    /// The mapper for the ChampionClass
+    /// </summary>
+    public static EnumsMapper<ChampionClass, ChampionClassVM> ChampionClassMapper { get; } = new(
+        (ChampionClass.Unknown, ChampionClassVM.Unknown), 
+        (ChampionClass.Assassin, ChampionClassVM.Assassin),
+        (ChampionClass.Fighter, ChampionClassVM.Fighter),
+        (ChampionClass.Mage, ChampionClassVM.Mage),
+        (ChampionClass.Marksman, ChampionClassVM.Marksman),
+        (ChampionClass.Support, ChampionClassVM.Support),
+        (ChampionClass.Tank, ChampionClassVM.Tank)
+        );
+    
+    /// <summary>
+    /// The mapper for the SkillType
+    /// </summary>
+    public static EnumsMapper<SkillType, SkillTypeVM> SkillTypeMapper { get; } = new(
+        (SkillType.Unknown, SkillTypeVM.Unknown),
+        (SkillType.Basic, SkillTypeVM.Basic),
+        (SkillType.Passive, SkillTypeVM.Passive),
+        (SkillType.Ultimate, SkillTypeVM.Ultimate)
+        );
+}
---
+++ Sources/ViewModel/Mappers/EnumsExtensions.cs
@@ -0,0 +1,37 @@
+﻿using Model;
+using ViewModel.Enums;
+
+namespace ViewModel.Mappers;
+
+internal static class ViewModelEnumExtensions
+{
+
+    ///<summary>
+    /// ChampionClassVM version of ToVM
+    ///</summary>
+    ///<param name="model">The model</param>
+    ///<returns>The view model</returns>
+    public static ChampionClassVM ToVM(this ChampionClass model) => EnumsMapper.ChampionClassMapper.GetOther(model);
+    
+    ///<summary>
+    /// SkillTypeVM version of ToVM
+    ///</summary>
+    ///<param name="model">The model</param>
+    ///<returns>The view model</returns>
+    public static SkillTypeVM ToVM(this SkillType model) => EnumsMapper.SkillTypeMapper.GetOther(model);
+    
+    /// <summary>
+    /// SkillType version of ToModel
+    /// </summary>
+    /// <param name="vm">The view model</param>
+    /// <returns>The model</returns>
+    public static SkillType ToModel(this SkillTypeVM vm) => EnumsMapper.SkillTypeMapper.GetBase(vm);
+    
+    /// <summary>
+    /// ChampionClass version of ToModel
+    /// </summary>
+    /// <param name="vm">The view model</param>
+    /// <returns>The model</returns>
+    public static ChampionClass ToModel(this ChampionClassVM vm) => EnumsMapper.ChampionClassMapper.GetBase(vm);
+    
+}
