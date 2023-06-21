345701e3c9894e6284baffb9da0306789ce1f67c
Samuel SIRVEN <samuel.sirven@etu.uca.fr>
2023-06-21 16:06:36 +0200

    💄 start adding the view for skills

---
+++ Sources/LoLApp/UI/Views/SkillsEditView.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LoLApp.ViewModel;
+
+namespace LoLApp.UI.Views;
+
+public partial class SkillsEditView : ContentView
+{
+    public  EditApplicationChampionVM EditAppVM
+    {
+        get => (EditApplicationChampionVM)GetValue(EditAppVMProperty);
+        set => SetValue(EditAppVMProperty, value);
+    }
+    public static readonly BindableProperty EditAppVMProperty =
+        BindableProperty.Create(nameof(EditAppVM), typeof(EditApplicationChampionVM), typeof(SkillsEditView), null);
+    
+    public SkillsEditView()
+    {
+        InitializeComponent();
+    }
+}
+
